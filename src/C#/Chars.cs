e874040006b8ad947de83fe53f3660f2dbf534e1
Nathan2076 <68337317+Nathan2076@users.noreply.github.com>
2023-06-23 21:24:04 -0300

    lots of stuff

---
+++ src/C#/Chars.cs
@@ -0,0 +1,10 @@
+namespace SharpPassword
+{
+    static class Chars
+    {
+        public static char[] uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        public static char[] lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        public static char[] numbers = "0123456789".ToCharArray();
+        public static char[] symbols = "!@#$%^&*()-_=+[]{}|\\:;\"'<>,.?/´`".ToCharArray();
+    }
+}
